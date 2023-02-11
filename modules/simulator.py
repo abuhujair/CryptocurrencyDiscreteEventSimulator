362c630187b56600dd7b3faf7606ca1e60ecfd70
sks-shubham <shubhamkumarsonkar1999@gmail.com>
2023-02-11 16:38:51 +0530

    added hash function

--- modules/simulator.py
+++ modules/simulator.py
@@ -12,10 +12,11 @@ from modules.blockchain import Block, Transaction
 class Simulator:
     """Simulator instance
     """
-    def __init__(self, num_nodes:int , slow_nodes:float, inter_arrival_time:float,
+    def __init__(self, num_nodes:int , slow_nodes:float, low_hash:float, inter_arrival_time:float,
                     inter_arrival_time_block:float , simulation_time:float, MAX_BLOCK_LENGTH: int):
         self.num_nodes = num_nodes
         self.num_slow_nodes = int(num_nodes*slow_nodes)
+        self.num_low_hash = int(num_nodes*low_hash)
         self.iat = inter_arrival_time
         self.iat_block = inter_arrival_time_block
         self.MAX_BLOCK_LENGTH = MAX_BLOCK_LENGTH
@@ -83,24 +84,41 @@ class Simulator:
         )
 
         nodes = {}
-        random_index = random.sample(range(self.num_nodes),self.num_nodes)
 
-        hash = list(self.gen_exp.uniform(0.0,1.0,self.num_nodes))
-        hash = [i/sum(hash) for i in hash]
+        hash_power = 1/(10*self.num_nodes - 9*self.num_low_hash)
 
-        for i in random_index[0:self.num_slow_nodes]:   # SLOW nodes
-            nodes[i] = Node(node_id=i, 
-                            node_type=0, 
-                            genesis_block=copy.deepcopy(genesis_block),
-                            hash = hash[i],
-                            MAX_BLOCK_LENGTH=self.MAX_BLOCK_LENGTH)
-        
-        for i in random_index[self.num_slow_nodes:self.num_nodes]:  # FAST nodes
-            nodes[i] = Node(node_id=i, 
-                            node_type=1, 
-                            genesis_block=copy.deepcopy(genesis_block), 
-                            hash = hash[i],
-                            MAX_BLOCK_LENGTH=self.MAX_BLOCK_LENGTH)
+        slow_nodes = random.sample(range(self.num_nodes), self.num_slow_nodes)
+        low_hash_p = random.sample(range(self.num_nodes), self.num_low_hash)
+
+        for i in range(self.num_nodes):
+            if (i in slow_nodes) and (i in low_hash_p):
+                nodes[i] = Node(node_id=i,
+                                node_type=0,
+                                genesis_block=copy.deepcopy(genesis_block),
+                                hash=hash_power,
+                                MAX_BLOCK_LENGTH=self.MAX_BLOCK_LENGTH)
+            
+            elif i in slow_nodes:
+                nodes[i] = Node(node_id=i,
+                                node_type=0,
+                                genesis_block=copy.deepcopy(genesis_block),
+                                hash=hash_power*10,
+                                MAX_BLOCK_LENGTH=self.MAX_BLOCK_LENGTH)
+
+            elif i in low_hash_p:
+                nodes[i] = Node(node_id=i,
+                                node_type=1,
+                                genesis_block=copy.deepcopy(genesis_block),
+                                hash=hash_power,
+                                MAX_BLOCK_LENGTH=self.MAX_BLOCK_LENGTH)
+            
+            else:
+                nodes[i] = Node(node_id=i,
+                                node_type=1,
+                                genesis_block=copy.deepcopy(genesis_block),
+                                hash=hash_power*10,
+                                MAX_BLOCK_LENGTH=self.MAX_BLOCK_LENGTH)
+
         return nodes
 
     def create_network(self):
